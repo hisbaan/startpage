f5042d0000e6e19e230ae42d2a78cebef21a24fd
hisbaan <hisbaan@gmail.com>
2025-04-14 11:50:30 -0400

    Update links and fix flex layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,6 @@ const links = [
       { title: "gitlab", url: "https://gitlab.com" },
       { title: "gmail", url: "https://mail.google.com" },
       { title: "meet", url: "https://meet.google.com" },
-      { title: "youtube", url: "https://youtube.com" },
     ],
   },
   {
@@ -20,17 +19,21 @@ const links = [
       },
       { title: "docs", url: "https://app.clickup.com/9014374112/docs" },
       {
-        title: "review",
-        url: "https://gitlab.com/groups/cocoflo-dev/-/merge_requests",
+        title: "figma",
+        url: "https://www.figma.com/design/WMXzoJU8DZox68bW8QBQqp/REC--Wireframes",
       },
       {
-        title: "cocoflo-api",
-        url: "https://gitlab.com/cocoflo-dev/cocoflo-api/-/merge_requests",
+        title: "p&r",
+        url: "https://gitlab.com/cocoflo-dev/parks-and-rec/-/merge_requests",
       },
-      {
-        title: "webclient",
-        url: "https://gitlab.com/cocoflo-dev/webclient/-/merge_requests",
-      },
+      // {
+      //   title: "cocoflo-api",
+      //   url: "https://gitlab.com/cocoflo-dev/cocoflo-api/-/merge_requests",
+      // },
+      // {
+      //   title: "webclient",
+      //   url: "https://gitlab.com/cocoflo-dev/webclient/-/merge_requests",
+      // },
     ],
   },
 ];
@@ -40,12 +43,17 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-center">
       <div className="text-white border border-neutral-400 rounded-lg p-7 flex flex-col gap-4">
         <DateComponent />
-        <div className="flex justify-center items-center gap-10">
+        <div className="flex justify-center items-start gap-10">
           {links.map((section) => (
             <div key={section.header} className="flex flex-col">
               <div className="underline">{section.header}</div>
               {section.items.map((item) => (
-                <a className="hover:text-neutral-300 transition-all" target="_top" key={item.title} href={item.url}>
+                <a
+                  className="hover:text-neutral-300 transition-all"
+                  target="_top"
+                  key={item.title}
+                  href={item.url}
+                >
                   {item.title}
                 </a>
               ))}
